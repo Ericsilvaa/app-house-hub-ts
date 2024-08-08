6a262502b09eb1c23887e79340a098a01be1bd8c
Eric <eriicsilva28@gmail.com>
2024-08-08 17:55:55 -0300

    feat: api

---
+++ src/api/data/seed.ts
@@ -0,0 +1,24 @@
+import { env } from '@/utils/env'
+import { getItem, setItem } from '@/utils/localStorage'
+import { listings } from './listings'
+import { locations } from './locations'
+import { users } from './users'
+
+// Add all data to localstorage to simulate database
+export const seedLocalDatabase = () => {
+  const database = getItem(env.DB_KEY)
+
+  // If a database already exists, do nothing
+  if (database) {
+    return
+  }
+
+  // Creates the initial database with all data
+  const initialDatabase = {
+    listings,
+    locations,
+    users
+  }
+
+  setItem(env.DB_KEY, initialDatabase)
+}
